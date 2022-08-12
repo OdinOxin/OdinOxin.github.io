eb4170b66ad661e4dcc8e9e6f8b113f3652b6f72
OdinOxin <OdinOxin@googlemail.com>
2022-08-12 19:22:06 +0200

    touch-action

--- scripts.js
+++ scripts.js
@@ -277,6 +277,7 @@ function getSignSvg(root, uuid, unit, x, y) {
     signSvg.setAttribute('uuid', uuid);
     signSvg.classList.add('draggable');
     signSvg.innerHTML = getSign(root['func'], unit);
+    signSvg.childNodes[0].setAttribute('touch-action', 'none');
     signSvg.childNodes[0].setAttribute('onpointerover', `pointerOverSvg('${uuid}')`);
     signSvg.childNodes[0].setAttribute('onpointerout', `pointerOutSvg('${uuid}')`);
     return signSvg;
