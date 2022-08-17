04aed3b43c79a0e4fce057d4502b143af547ab6a
Lucas Weckermann <lweckermann@lis.eu>
2022-08-17 10:30:05 +0200

    at least one row

--- scripts.js
+++ scripts.js
@@ -510,7 +510,7 @@ function drawRecursive(canvas, root, layer, x, y) {
             canvas.appendChild(getLine(x + GAP + colWith * signWidth, y + signHeight / 2, x + GAP + colWith * signWidth, y + rowLineEnd * signHeight + signHeight / 2));
         }
     }
-    return rowSub;
+    return Math.max(1, rowSub);
 }
 
 function draw() {
